fe0d735df3946055b50a66666b7401f6e69787c0
Guillaume LADORME <gladorme@gmail.com>
2026-08-16 18:19:51 +0200

    Fix CI
    Signed-off-by: Guillaume LADORME <gladorme@gmail.com>

--- dashboards/src/components/GridLayout/GridLayout.tsx
+++ dashboards/src/components/GridLayout/GridLayout.tsx
@@ -1,4 +1,3 @@
-import { useVariableValues, VariableContext, PanelGroupId } from '@perses-dev/plugin-system';
 // Copyright The Perses Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +10,8 @@ import { useVariableValues, VariableContext, PanelGroupId } from '@perses-dev/pl
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+import { useVariableValues, VariableContext, PanelGroupId } from '@perses-dev/plugin-system';
 import { ReactElement, useState } from 'react';
 import { Layout, Layouts } from 'react-grid-layout';
 
