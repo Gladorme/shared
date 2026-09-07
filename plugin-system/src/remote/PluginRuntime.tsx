e822aeb344e12b8613a7900cd269357ed33ca275
Guillaume LADORME <Gladorme@users.noreply.github.com>
2026-09-07 16:17:10 +0200

    spec too
    Signed-off-by: Guillaume LADORME <Gladorme@users.noreply.github.com>

--- plugin-system/src/remote/PluginRuntime.tsx
+++ plugin-system/src/remote/PluginRuntime.tsx
@@ -129,11 +129,11 @@ const getPluginRuntime = (): ModuleFederation => {
           },
         },
         '@perses-dev/spec': {
-          version: '0.3.0-beta.5',
+          version: '0.3.0-beta.7',
           lib: () => getHostSharedModule('@perses-dev/spec'),
           shareConfig: {
             singleton: true,
-            requiredVersion: '^0.3.0-beta.5',
+            requiredVersion: '^0.3.0-beta.7',
           },
         },
         '@perses-dev/client': {
